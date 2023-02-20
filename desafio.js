fdbf0db6d7b77c62f836b7fb3013a7b877f8ce61
Demian <demparom@gmail.com>
2023-02-20 13:21:18 -0300

    1er commit

--- desafio.js
+++ desafio.js
@@ -1,81 +1,82 @@
 class ProductManager {
-    constructor() {
-        this.products = [];
-    }
+  constructor() {
+    this.products = [];
+  }
 
-    getProducts = () => {
-        console.log(this.products);
-        return;
-      };
+  getProducts = () => {
+    console.log(this.products);
+    return;
+  };
 
-    createProduct = (title, description, price, thumbnail, code, stock) => {
-        const product = {
-            id: this.products.length + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        };
-
-    this.products.push(product);
+  createProduct = (title, description, price, thumbnail, code, stock) => {
+    const product = {
+      id: this.products.length + 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
     };
 
+    this.products.push(product);
+  };
 
-addProduct = (title1, description1, price1, thumbnail1, code1, stock1) => {
-  const productIndex = this.products.findIndex((product) => product.code === code1);
+  addProduct = (title, description, price, thumbnail, code, stock) => {
+    const productIndex = this.products.findIndex(
+      (product) => product.code === code
+    );
 
-  const productoAgregar = {
-    id: this.products.length + 1,
-    title1,
-    description1,
-    price1,
-    thumbnail1,
-    code1,
-    stock1
-};
+    const productoAgregar = {
+      id: this.products.length + 1,
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
 
-if(title1 === undefined ||
-   description1 === undefined ||
-   price1 === undefined ||
-   thumbnail1 === undefined ||
-   code1 === undefined ||
-   stock1 === undefined) {console.log("Falta un campo requerido")} else {
+    if (
+      title === undefined ||
+      description === undefined ||
+      price === undefined ||
+      thumbnail === undefined ||
+      code === undefined ||
+      stock === undefined
+    ) {
+      console.log("Falta un campo requerido");
+    } else {
+      const codeExists = this.products[productIndex]?.code === code;
 
-  const codeExists = 
-
-    this.products[productIndex]?.code === code1;
-
-  if (codeExists) {
-    console.log("Existe un código igual ya registrado!");
-    return;
-  } else {
-  this.products.push(productoAgregar);
-   }
-};
-};
-
-
-getProductsById = (id) => {
-    let productId = id;
-    const encontrado = this.products.find(element => element.id === productId);
-    if(encontrado) {
-      console.log(encontrado);}
-      else {
-        console.log('Not found');
+      if (codeExists) {
+        console.log("Existe un código igual ya registrado!");
+        return;
+      } else {
+        this.products.push(productoAgregar);
       }
+    }
+  };
+
+  getProductsById = (id) => {
+    let productId = id;
+    const encontrado = this.products.find(
+      (element) => element.id === productId
+    );
+    if (encontrado) {
+      console.log(encontrado);
+    } else {
+      console.log("Not found");
+    }
     return;
   };
 }
 
+/* Testing */
 const productManager = new ProductManager();
-
-
-productManager.createProduct("Caja", "Una caja", 150, "a.jpg", 1, 20);
-productManager.createProduct("Oso", "Un oso de peluche", 200, "b.jpg", 2, 20);
-productManager.addProduct("Caja", "Una caja", 150, "a.jpg", 5, 20);
 productManager.getProducts();
+productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+productManager.getProducts();
+productManager.addProduct("producto 2", "Este es otro producto prueba", 300, "Sin imagen", "abc123", 25);
 productManager.getProductsById(5);
-
 
